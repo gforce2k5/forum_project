3a33fc64a3d6d27235ce2f2a7ab1f7081dabe719
Guy Even <gforce2k5@gmail.com>
2019-03-28 00:25:17 +0200

    final schema

--- schema/forum_project.sql
+++ schema/forum_project.sql
@@ -2,17 +2,10 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
-<<<<<<< HEAD
 -- Host: localhost
 -- Generation Time: Mar 27, 2019 at 11:19 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
-=======
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 26, 2019 at 07:21 PM
--- Server version: 5.7.24
--- PHP Version: 7.2.14
->>>>>>> 16dded6e5ffe5d7e9202225a285b57466ddaac65
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -35,13 +28,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `categories`
 --
 
-<<<<<<< HEAD
-CREATE TABLE `categories` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` text NOT NULL,
-  `cat_order` int(11) NOT NULL,
-  `active` tinyint(1) NOT NULL
-=======
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -49,7 +35,6 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `cat_order` int(11) NOT NULL,
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
->>>>>>> 16dded6e5ffe5d7e9202225a285b57466ddaac65
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -58,25 +43,16 @@ CREATE TABLE IF NOT EXISTS `categories` (
 -- Table structure for table `forums`
 --
 
-<<<<<<< HEAD
-CREATE TABLE `forums` (
-  `id` int(10) UNSIGNED NOT NULL,
-=======
 DROP TABLE IF EXISTS `forums`;
 CREATE TABLE IF NOT EXISTS `forums` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
->>>>>>> 16dded6e5ffe5d7e9202225a285b57466ddaac65
   `topic` text NOT NULL,
   `description` text NOT NULL,
   `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `cat_order` int(11) NOT NULL,
   `cat_id` int(10) UNSIGNED NOT NULL,
-<<<<<<< HEAD
-  `active` tinyint(1) NOT NULL
-=======
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
->>>>>>> 16dded6e5ffe5d7e9202225a285b57466ddaac65
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -88,12 +64,8 @@ CREATE TABLE IF NOT EXISTS `forums` (
 CREATE TABLE `forum_managers` (
   `id` int(10) UNSIGNED NOT NULL,
   `user_id` int(11) NOT NULL,
-<<<<<<< HEAD
-  `forum_id` int(11) NOT NULL
-=======
   `forum_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
->>>>>>> 16dded6e5ffe5d7e9202225a285b57466ddaac65
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -102,14 +74,9 @@ CREATE TABLE `forum_managers` (
 -- Table structure for table `posts`
 --
 
-<<<<<<< HEAD
-CREATE TABLE `posts` (
-  `id` int(10) UNSIGNED NOT NULL,
-=======
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE IF NOT EXISTS `posts` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
->>>>>>> 16dded6e5ffe5d7e9202225a285b57466ddaac65
   `title` text NOT NULL,
   `content` mediumtext NOT NULL,
   `creation_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -118,12 +85,8 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `forum_id` int(10) UNSIGNED DEFAULT NULL,
   `post_id` int(10) UNSIGNED DEFAULT NULL,
   `is_pinned` tinyint(1) NOT NULL,
-<<<<<<< HEAD
-  `is_locked` tinyint(1) NOT NULL
-=======
   `is_locked` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
->>>>>>> 16dded6e5ffe5d7e9202225a285b57466ddaac65
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -134,10 +97,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
 
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
-<<<<<<< HEAD
   `username` tinytext NOT NULL,
-=======
->>>>>>> 16dded6e5ffe5d7e9202225a285b57466ddaac65
   `first_name` text NOT NULL,
   `last_name` text NOT NULL,
   `password` varchar(64) NOT NULL,
@@ -148,76 +108,9 @@ CREATE TABLE `users` (
   `signature` mediumtext NOT NULL,
   `email` text NOT NULL,
   `hash` varchar(64) DEFAULT NULL,
-<<<<<<< HEAD
-  `is_verified` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `forums`
---
-ALTER TABLE `forums`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `forum_managers`
---
-ALTER TABLE `forum_managers`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `forums`
---
-ALTER TABLE `forums`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `forum_managers`
---
-ALTER TABLE `forum_managers`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-=======
   `is_verified` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
->>>>>>> 16dded6e5ffe5d7e9202225a285b57466ddaac65
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
