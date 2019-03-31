1739e2d3cf61ea96d93e119243917d5c1690798a
Galia Appel <galia.appel@gmail.com>
2019-03-31 18:53:58 +0300

    register tamplate page

---
+++ templates/register.php
@@ -0,0 +1,10 @@
+<form action="functions/register.php" method="post">
+  <input type="text" name="username" placeholder="username">
+  <input type="text" name="first_name" placeholder="first_name">
+  <input type="text" name="last_name" placeholder="last_name">
+  <input type="email" name="email" placeholder="email">
+  <input type="password" name="password" placeholder="password">
+  <input type="password" name="confirmpwd" placeholder="once more">
+  <input type="checkbox" name="divur">
+  <input type="submit" name="Submit">
+</form>
