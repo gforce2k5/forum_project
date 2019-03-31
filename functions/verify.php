6dc37d190da096812cf667585a0e6bc677a85151
Guy Even <gforce2k5@gmail.com>
2019-03-31 10:08:41 +0300

    adding reset password option

--- functions/verify.php
+++ functions/verify.php
@@ -5,11 +5,12 @@
     $sql = new SQL($link, "SELECT id FROM users WHERE hash = '$hash' AND registr_date > {time() - 24 * 60 * 60} LIMIT 1");
 
     if ($sql->rows() >= 1) {
+      echo $sql->rows();
       $id = $sql->result()['id'];
       $sql = new SQL($link, "UPDATE users SET hash = NULL, is_verified = 1 WHERE id = '$id' LIMIT 1");
 
       if ($sql->is_ok()) {
-        echo 'You Are Verified';
+        header("location:../index.php");
       }
     } else {
       header("location:../index.php");
