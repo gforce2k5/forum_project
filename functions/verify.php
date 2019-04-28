413ba08414e7f41acf7cf9a398e5283ecfeb6a1d
Guy Even <gforce2k5@gmail.com>
2019-04-28 20:20:51 +0300

    fixed bug in verify script

--- functions/verify.php
+++ functions/verify.php
@@ -4,8 +4,9 @@
     $hash = mysqli_real_escape_string($link, $_GET['hash']);
     $id = mysqli_real_escape_string($link, $_GET['id']);
 
-    $sql = new SQL($link, "UPDATE users SET hash = NULL, ststus = 1 WHERE id = '$id' AND ".
-      "hash = '$hash' AND register_date > {sql_time(time() - 24 * 60 * 60)} LIMIT 1");
+    $sql = new SQL($link, "UPDATE users SET hash = NULL, status = 1 WHERE id = '$id' AND ".
+      "hash = '$hash' AND register_date > '{sql_time(time() - 24 * 60 * 60)}' LIMIT 1");
+    print_array($sql);
   }
   header("location:../index.php");
 ?>
